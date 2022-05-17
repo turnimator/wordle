baf3049992389d4a88d18fd271da5784fe76e44f
Jan Atle Ramsli <jaramsli@gmail.com>
2022-05-17 22:44:59 +0200

    Improved readability
    Added a couple of newlines for readability

--- wordle-reduced/main.cpp
+++ wordle-reduced/main.cpp
@@ -6,15 +6,15 @@ int main()
     std::string my_guess;
     
     for (int i = 0; i < 6; i++){
-        std::cout << "Guess word: ";
+        std::cout << "\nGuess word: ";
         std::cin >> my_guess;
         std::cout << "\nYou guessed " << my_guess << "\n";
         GuessResponse response = wordle.guess(my_guess);
         if (response.first){
-            std::cout << "YOU WIN!!!\n";
+            std::cout << "\nYOU WIN!!!\n";
             break;
         } else {
-            std::cout << 5 - i << " tries left.\n";
+            std::cout << "\nHmm ..." << 5 - i << " tries left.\n";
         }
     }
 }
